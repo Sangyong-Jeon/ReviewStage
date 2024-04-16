60dacf7aae1dba12e421a11de7e58b6039f2e9da
JeonSangYong <tkddyd420@naver.com>
2024-04-16 16:16:56 +0900

    refactor: csv to db 저장

--- reviewstage/performance/management/commands/read_csv_performance.py
+++ reviewstage/performance/management/commands/read_csv_performance.py
@@ -5,41 +5,43 @@ from performance.models import Performance
 
 # 명령어로 사용 -> python manage.py read_csv_performance csv파일경로
 class Command(BaseCommand):
-	help = 'CSV 파일을 DB로 이전합니다.'
+    help = 'CSV 파일을 DB로 이전합니다.'
 
-	def add_arguments(self, parser):
-		parser.add_argument('csv_file', type=str)
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str)
 
-	def handle(self, *args, **kwargs):
-		csv_file = kwargs['csv_file']
-		try:
-			with open(csv_file, 'r') as file:
-				reader = csv.DictReader(file)
+    def handle(self, *args, **kwargs):
+        csv_file = kwargs['csv_file']
+        try:
+            self.import_data(csv_file)
+            self.stdout.write(self.style.SUCCESS('CSV 파일 이전 완료'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR('File not found. Please check the file again.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'An unknown error occurred: {e}'))
 
-				for row in reader:
-					try:
-						performance = Performance.objects.create(
-							performance_num=int(row['performance_num']),
-							title=row['title'],
-							location=row['location'],
-							start_date=row['start_date'],
-							end_date=row['end_date'],
-							performance_time=row['performance_time'],
-							age_requirement=row['age_requirement'],
-							performance_type=row['performance_type'],
-							image_path=row['image_path']
-						)
-						# file_instance = File.objects.create(
-						# 	performance_id=performance,
-						# 	name=row['name'],
-						# 	store_path=row['store_path']
-						# )
-					except Exception as e:
-						self.stdout.write(self.style.ERROR(f'Error: {row}'))
-						self.stdout.write(self.style.ERROR(f'Error detail: {e}'))
-		except FileNotFoundError:
-			self.stdout.write(self.style.ERROR('File not found. Please check the file again.'))
-		except Exception as e:
-			self.stdout.write(self.style.ERROR(f'An unknown error occurred.{e}'))
+    def import_data(self, csv_file):
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.create_performance(row)
 
-		self.stdout.write(self.style.SUCCESS('CSV 파일 이전 완료'))
+    def create_performance(self, data):
+        try:
+            Performance.objects.create(
+                performance_num=int(data['performance_num']),
+                title=data['title'],
+                location=data['location'],
+                start_date=data['start_date'],
+                end_date=data['end_date'],
+                performance_time=data['performance_time'],
+                age_requirement=data['age_requirement'],
+                performance_type=data['performance_type'],
+                image_path=data['image_path']
+            )
+        except Exception as e:
+            self.handle_error(data, e)
+
+    def handle_error(self, data, exception):
+        self.stdout.write(self.style.ERROR(f'Error: {data}'))
+        self.stdout.write(self.style.ERROR(f'Error detail: {exception}'))
--- reviewstage/performance/management/commands/read_csv_review.py
+++ reviewstage/performance/management/commands/read_csv_review.py
@@ -1,40 +1,47 @@
 import csv
 from django.core.management.base import BaseCommand
-from performance.models import Review
+from performance.models import Review, Performance
 
 
 # 명령어로 사용 -> python manage.py read_csv_review csv파일경로
 class Command(BaseCommand):
-	help = 'CSV 파일을 DB로 이전합니다.'
+    help = 'CSV 파일을 DB로 이전합니다.'
 
-	def add_arguments(self, parser):
-		parser.add_argument('csv_file', type=str)
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str)
 
-	def handle(self, *args, **kwargs):
-		csv_file = kwargs['csv_file']
-		try:
-			with open(csv_file, 'r') as file:
-				reader = csv.DictReader(file)
+    def handle(self, *args, **kwargs):
+        csv_file = kwargs['csv_file']
+        try:
+            self.import_data(csv_file)
+            self.stdout.write(self.style.SUCCESS('CSV 파일 이전 완료'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR('File not found. Please check the file again.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'An unknown error occurred: {e}'))
 
-				for row in reader:
-					try:
-						review = Review.objects.create(
-							performance_id=int(row['performance_num']),
-							review_num=int(row['review_num']),
-							title=row['title'],
-							content=row['content'],
-							user_id=row['user_id'],
-							rating=int(row['rating']),
-							date=row['date'],
-							likes_count=int(row['likes_count']),
-							view_count=int(row['view_count'])
-						)
-					except Exception as e:
-						self.stdout.write(self.style.ERROR(f'Error: {row}'))
-						self.stdout.write(self.style.ERROR(f'Error detail: {e}'))
-		except FileNotFoundError:
-			self.stdout.write(self.style.ERROR('File not found. Please check the file again.'))
-		except Exception as e:
-			self.stdout.write(self.style.ERROR(f'An unknown error occurred.{e}'))
+    def import_data(self, csv_file):
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.create_review(row)
 
-		self.stdout.write(self.style.SUCCESS('CSV 파일 이전 완료'))
+    def create_review(self, data):
+        try:
+            Review.objects.create(
+                performance_id=Performance.objects.get(performance_num=data['performance_num']),
+                review_num=int(data['review_num']),
+                title=data['title'],
+                content=data['content'],
+                user_id=data['user_id'],
+                rating=int(data['rating']),
+                date=data['date'],
+                likes_count=int(data['likes_count']),
+                view_count=int(data['view_count'])
+            )
+        except Exception as e:
+            self.handle_error(data, e)
+
+    def handle_error(self, data, exception):
+        self.stdout.write(self.style.ERROR(f'Error: {data}'))
+        self.stdout.write(self.style.ERROR(f'Error detail: {exception}'))
