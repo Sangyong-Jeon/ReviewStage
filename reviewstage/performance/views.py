3a89577091620794a270a2d7db64113548be7832
JeonSangYong <tkddyd420@naver.com>
2024-04-18 17:52:39 +0900

    feat: 홈페이지

--- reviewstage/performance/views.py
+++ reviewstage/performance/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world.")
+class HomeView(TemplateView):
+    template_name = 'home.html'
